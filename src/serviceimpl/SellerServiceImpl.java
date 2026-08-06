67fed170fb2dd32ce13e1527332c37b1245e51c8
cj_dev <cj2349@naver.com>
2026-08-07 00:10:33 +0900

    ServiceImpl delteBuyer, setBuyer 변경점 반영

--- src/serviceimpl/SellerServiceImpl.java
+++ src/serviceimpl/SellerServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 
 import domain.Product;
 import domain.Seller;
