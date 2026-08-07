3d9d7d6663ac7502329569d11f2fedf5d8e9a2aa
cj_dev <cj2349@naver.com>
2026-08-07 12:33:50 +0900

    0807 충돌 해결 commit

--- src/serviceimpl/BuyerServiceImpl.java
+++ src/serviceimpl/BuyerServiceImpl.java
@@ -23,7 +23,6 @@ public class BuyerServiceImpl implements BuyerService {
 	private List<Order> orderList;
 
 
-
 	public BuyerServiceImpl() {
 		this.buyerMap = new HashMap<>();
 		this.orderList = new ArrayList<>();
@@ -108,7 +107,7 @@ public class BuyerServiceImpl implements BuyerService {
 
 	@Override
 	public void modifyBuyer(Buyer buyer) {
-		this.setBuyer(buyer, "탈퇴한 사용자 수정 발생", () -> true,  () -> {
+		this.setBuyer(buyer, "탈퇴한 사용자 수정 발생", () -> true, () -> {
 			// FIXME 입력 유효성 검사 유무 확인 필요
 			buyer.setName(Reader.readString("변경할 이름: "));
 			buyer.setPassword(Reader.readString("변경할 비밀번호: "));
@@ -119,9 +118,8 @@ public class BuyerServiceImpl implements BuyerService {
 
 	@Override
 	public void deleteBuyer(Buyer buyer) {
-		this.setBuyer(buyer, "탈퇴한 사용자 재탈퇴 시도 발생"
-					 , () -> buyer.getUserId() == Reader.readString("아이디를 입력하세요: ") 
-					 , () -> buyer.setActive(false));
+		this.setBuyer(buyer, "탈퇴한 사용자 재탈퇴 시도 발생", () -> buyer.getUserId() == Reader.readString("아이디를 입력하세요: "),
+				() -> buyer.setActive(false));
 	}
 
     @Override
@@ -153,10 +151,11 @@ public class BuyerServiceImpl implements BuyerService {
 		if (!buyer.isActive()) {
 			throw new IllegalStateException(err);
 		}
+
 		if (!check.get()) {
 			System.out.println("아이디가 틀렸습니다.");
 		}
-		
+
 		String password = Reader.readString("비밀번호를 입력해주세요: ");
 		if (buyer.getPassword().equals(password)) {
 			action.run();
